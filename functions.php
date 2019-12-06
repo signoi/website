5d975ae2f2851922a098f6aa935cb5467dd0ea9b
fionchadd <hello@cogdiss.com>
2019-12-06 14:26:13 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -120,9 +120,7 @@ add_action( 'widgets_init', 'signoi_theme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function signoi_theme_scripts() {
-	wp_enqueue_style( 'signoi-theme-style', get_stylesheet_uri() );
-
-	wp_enqueue_style( 'signoi-theme-sass', get_template_directory_uri() . '/sass/style.css', array(), '20151215', true );
+	wp_enqueue_style( 'signoi-theme-style', get_template_directory_uri() . '/sass/style.css');
 
 	wp_enqueue_script( 'signoi-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
