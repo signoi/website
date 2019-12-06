e827db491e552a3b393b9dbbde32237c905edffd
fionchadd <hello@cogdiss.com>
2019-12-06 16:29:17 +0000

    path changes
    - changed path to flexslider
    - changed path to quotemarks (nb look at moving SVGs inside theme file perhaps?)

--- footer.php
+++ footer.php
@@ -31,6 +31,6 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-<script type="text/javascript" src="jquery.flexslider-min.js"></script>
+<script type="text/javascript" src="/wp-content/themes/signoi-theme/js/jquery.flexslider-min.js"></script>
 </body>
 </html>
