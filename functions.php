df09a92b0b8d56eef765fa79580cbf834234095d
fionchadd <hello@cogdiss.com>
2019-12-06 14:22:10 +0000

    Update functions.php
    Not sure why but sass compiler is compiling into /sass/style.css rather than /style.css so I've enqueued the /sass/style.css

--- functions.php
+++ functions.php
@@ -122,6 +122,8 @@ add_action( 'widgets_init', 'signoi_theme_widgets_init' );
 function signoi_theme_scripts() {
 	wp_enqueue_style( 'signoi-theme-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'signoi-theme-sass', get_template_directory_uri() . '/sass/style.css', array(), '20151215', true );
+
 	wp_enqueue_script( 'signoi-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'signoi-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
