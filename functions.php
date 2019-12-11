d7731d6b7955d5a4de5e190e63d1988ed5c9ca4c
fionchadd <hello@cogdiss.com>
2019-12-11 15:10:42 +0000

    I don't think it's loading the CSS properly

--- functions.php
+++ functions.php
@@ -122,7 +122,7 @@ add_action( 'widgets_init', 'signoi_theme_widgets_init' );
 function signoi_theme_scripts() {
 	wp_enqueue_style( 'signoi-theme-style', get_template_directory_uri() . '/sass/style.css');
 	wp_enqueue_style( 'flexslider-style', get_template_directory_uri() . '/sass/flexslider.css');
-	wp_enqueue_style( 'flexslider-style', get_template_directory_uri() . '/sass/jquery.flipster.min.css');
+	wp_enqueue_style( 'flexslider-style', get_template_directory_uri() . '/sass/flipster.min.css');
 	wp_enqueue_script( 'signoi-custom-scripts', get_template_directory_uri() . '/js/signoiscripts.js', array('jquery'), null, true);
 	wp_enqueue_script( 'signoi-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
