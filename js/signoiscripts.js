a2919ab7dc9309d71c440232a10dcd1403151373
fionchadd <hello@cogdiss.com>
2019-12-12 16:19:37 +0000

    Waypoints troubleshooting
    Adding consoe commands to try and troubleshoot

--- js/signoiscripts.js
+++ js/signoiscripts.js
@@ -6,26 +6,26 @@ jQuery(document).ready(function () {
       jQuery('body').toggleClass('menuisopen');
     });
 
-  });
-
-    jQuery(document).ready(function(){
         //set a waypoint for all the page parts on the page
         var sectionwaypoint = jQuery('section').waypoint(function() {
             //check the direction
                 //add the class to start the animation
                 jQuery(this.element).addClass('start');
+                console.log("section reached");
         }, {
             //Set the offset
             offset: '90%'
         });
         var semioticswaypoint = jQuery('#quantitative-semiotics .row').waypoint(function() {
               jQuery(this.element).addClass('start');
+              console.log("QS row reached");
       }, {
           //Set the offset
           offset: 'bottom-in-view'
       });
       var callbackwaypoint = jQuery('#callback-cta .row').waypoint(function() {
         jQuery(this.element).addClass('start');
+        console.log("CTA row reached");
 }, {
     //Set the offset
     offset: 'bottom-in-view'
