f7ce817dcb7238355af409ab552b724c712607c9
fionchadd <hello@cogdiss.com>
2019-12-12 15:06:50 +0000

    Blog styling & homepage media queries
    - addition of <section>, row and col structure for single post
    - addition of blog styling from current site
    - media queries for alternative homepage

--- single.php
+++ single.php
@@ -9,9 +9,9 @@
 
 get_header();
 ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<section>
+	<div id="primary" class="content-area row">
+		<main id="main" class="site-main col">
 
 		<?php
 		while ( have_posts() ) :
