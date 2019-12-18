bccee4205e036a515ee242a7c0c4b5f07ed8bdf8
fionchadd <hello@cogdiss.com>
2019-12-18 15:30:10 +0000

    styling of platform page

--- platform-page.php
+++ platform-page.php
@@ -44,7 +44,7 @@ $subtitle = get_sub_field('subheading');
 			$alt = $image['alt'];
 		
 			// Thumbnail size attributes.
-			$size = 'medium';
+			$size = 'large';
 			$fullimage = $image['sizes'][ $size ];
 		endif;
 		if( get_row_index() % 2 == 0 ){
@@ -53,8 +53,9 @@ $subtitle = get_sub_field('subheading');
 	<div class="row even">
 		<div class="col half image-col"><img src="<?php echo esc_url($fullimage); ?>" alt="<?php echo esc_attr($alt); ?>" /></div>
 		<div class="col half">
+			<p class="subheading"><?php echo $subtitle; ?></p>
 			<h2><?php echo $title; ?></h2>
-			<p><?php echo $copy; ?></p>
+			<?php echo $copy; ?>
 		</div>
 	</div>
 </section>
@@ -62,15 +63,31 @@ $subtitle = get_sub_field('subheading');
 <section class="platform-section">
 <div class="row odd">
 		<div class="col half">
+		<p class="subheading"><?php echo $subtitle; ?></p>
 			<h2><?php echo $title; ?></h2>
-			<p><?php echo $copy; ?></p>
+			<?php echo $copy; ?>
 		</div>
 		<div class="col half image-col"><img src="<?php echo esc_url($fullimage); ?>" alt="<?php echo esc_attr($alt); ?>" /></div>
 			</div>	
 </section>
 <?php } endwhile; else : endif; ?>
 <?php endwhile; else : endif; ?>
-
+<?php if( have_rows('cta_section') ): 
+		while( have_rows('cta_section') ): the_row(); 
+		$title = get_sub_field('section_title'); 
+		$copy = get_sub_field('section_copy'); 
+?>
+<section id="callback-cta">
+	<div class="row">
+		<div class="col full">
+			<h3><?php echo $title; ?></h3>	
+			<p><?php echo $copy; ?></p>
+			<button class="button red" onclick="window.location.href = '<?php echo $link; ?>';"><?php echo $cta; ?></button>
+			</div>
+		</div>
+	</div>
+</section>	
+<?php endwhile; else : endif; ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 	<?php
