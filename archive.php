a8489f03e0f54c0cdb76d212e96c6b2bfe8abc98
fionchadd <hello@cogdiss.com>
2020-02-04 13:58:13 +0000

    Update archive.php
    Setting archive view

--- archive.php
+++ archive.php
@@ -9,9 +9,9 @@
 
 get_header();
 ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<section class="no-sidebar">
+	<div id="primary" class="content-area full-width row">
+		<main id="main" class="site-main col full">
 
 		<?php if ( have_posts() ) : ?>
 
@@ -47,7 +47,17 @@ get_header();
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+	</section>
+<section class="mailing-list-sign-up blog-footer">
+<div class="row">
+<div class="col full mailing-list-inner">
+<div class="mailing-list-icon"><img class="alignnone wp-image-1640" src="http://new.signoi.com/wp-content/uploads/2019/10/signoi-logo-01-150x150.png" alt="" width="55" height="55" srcset="http://new.signoi.com/wp-content/uploads/2019/10/signoi-logo-01-150x150.png 150w, http://new.signoi.com/wp-content/uploads/2019/10/signoi-logo-01.png 300w" sizes="(max-width: 55px) 100vw, 55px"></div>
+<div class="mailing-list-text">Subscribe to our newsletter and get all this great content sent directly to your inbox.</div>
+<div class="mailing-list-trigger pum-trigger" style="cursor: pointer;">
+<div class="button red">Sign Me Up</div>
+</div>
+</div>
+</div>
+	</section>
 <?php
-get_sidebar();
 get_footer();
