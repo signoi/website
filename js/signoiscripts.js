12f1eb447ecbdef7f5a217ea30651ee7837ffb9f
fionchadd <hello@cogdiss.com>
2020-02-10 13:13:08 +0000

    mobile menu

--- js/signoiscripts.js
+++ js/signoiscripts.js
@@ -8,7 +8,7 @@ jQuery(window).load(function() {
     //adding class to submenu when header item is clicked
     jQuery('.menu-item-has-children a').on('click', function () {
   
-      jQuery(this.element).toggleClass('open');
+      jQuery(this).parent.toggleClass('open');
     });    
 
         //adding class to body when popup trigger is clicked
