308cdf4ff75b8189679ac78ef57b090ae6f1c6d6
fionchadd <hello@cogdiss.com>
2020-02-11 11:09:48 +0000

    scroll behaviour

--- js/signoiscripts.js
+++ js/signoiscripts.js
@@ -174,7 +174,7 @@ jQuery(document).ready(function(){
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       jQuery('html, body').animate({
-        scrollTop: $(hash).offset().top
+        scrollTop: jQuery(hash).offset().top
       }, 800, function(){
 
         // Add hash (#) to URL when done scrolling (default click behavior)
