59298d7fedc4019ee4bd188f6a078b5bb1081e58
fionchadd <hello@cogdiss.com>
2020-02-11 13:15:06 +0000

    trying negative offset

--- js/signoiscripts.js
+++ js/signoiscripts.js
@@ -174,6 +174,7 @@ var slides = document.querySelectorAll("section.stick");
 for (var i=0; i<slides.length; i++) {
   new ScrollMagic.Scene({
       triggerElement: slides[i],
+      offset: -95,
     })
     .setPin(slides[i], {pushFollowers: false})
     .addIndicators() // add indicators (requires plugin)
