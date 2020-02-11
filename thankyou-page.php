29bfdc065a96e7c654b5f8d2936b5a0883856b90
fionchadd <hello@cogdiss.com>
2020-02-11 15:15:25 +0000

    Adding recent posts to thank-you page

--- thankyou-page.php
+++ thankyou-page.php
@@ -91,6 +91,57 @@ $copy = get_sub_field('section_copy');
 	</div>
 </section>
 <?php endwhile; else : endif; ?>
+<?php
+$args = array(
+    'post_type' => 'post',
+	'post_status' => 'publish',
+	'posts_per_page' => 3,
+	'ignore_sticky_posts' => true,
+);
+$arr_posts = new WP_Query( $args );
+ 
+if ( $arr_posts->have_posts() ) : 
+	$post = $posts[0]; $c=0; ?>
+<section id="recent-posts">
+<div class="row">
+	<div class="col full"><h2>Read our latest content...</h2></div>
+</div>
+<div class="row">
+<?php while ( $arr_posts->have_posts() ) : 
+					$arr_posts->the_post();
+					$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'blog' );
+					$thumb_id = get_post_thumbnail_id();
+					$alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
+					$slug = get_post_field( 'post_name', get_post() );
+					$readingtime = types_render_field( 'reading-time', array() );
+					$author = types_render_field( 'author-name', array() );
+					
+					?>
+
+<div class="col third">
+<div class="single-blog-item">
+  <div class="blog-inner">
+    <a class="blog-link" href="<?php the_permalink(); ?>"></a>
+  <div class="blog-image"><img src="<?php echo $image[0]?>" alt="<?php echo $alt[0]?>"></div>
+	<div class="blog-text-area">
+  <div class="blog-category"><?php the_category( ' | ' ); ?></div>
+  <div class="reading-time"><i class="fal fa-clock" aria-hidden="true"></i> <?php echo $readingtime ?> min read</div>
+  <div class="blog-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+  <div class="blog-meta">
+  <?php if(	(empty($author)) ) { ?><span class="blog-author">by <?php the_author(); } ?></span> 
+  <?php if(	(!empty($author)) ) {  ?><span class="blog-author">by <?php echo $author; } ?></span> 
+ <span class="blog-date"><?php the_date(); ?></span></div>
+ </div>
+    </div>
+</div>
+</div>
+<?php endwhile; ?>
+</div>
+</section>
+<?php 
+endif;
+wp_reset_postdata();
+?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
