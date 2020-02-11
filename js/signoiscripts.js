ef4006835f66f3239a18c6b0c6da31a7364928e5
fionchadd <hello@cogdiss.com>
2020-02-11 13:19:21 +0000

    Trying to manually set height

--- js/signoiscripts.js
+++ js/signoiscripts.js
@@ -161,7 +161,7 @@ jQuery('.flipster').flipster({
 var controller = new ScrollMagic.Controller({
   globalSceneOptions: {
     triggerHook: 'onLeave',
-    duration: "100%" // this works just fine with duration 0 as well
+    duration: "650" // this works just fine with duration 0 as well
     // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
     // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
   }
